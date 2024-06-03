c1f6ea6cc28dfdfcd11f24dc2d698eb2cd36f8bf
Bifrost <nkeyani-@student.42barcelona.com>
2024-06-03 17:24:30 +0200

    feat: parsing password on ircserv

--- src/irc.cpp
+++ src/irc.cpp
@@ -1,20 +1,20 @@
 #include "client/Client.hpp"
 #include "server/Server.hpp"
+#include <cstdlib>
 #include <exception>
 #include <iostream>
 #include <signal.h>
 
 bool powerOn = true;
 
-void sigHandler(int sigNum) 
-{
+void sigHandler(int sigNum) {
     (void)sigNum;
-	powerOn = false;
+    powerOn = false;
 }
 
 int main(int argc, char **argv) {
     if (argc != 3) {
-        std::cout << "Error: use ./ircserv <port> <password>" << std::endl;
+        std::cerr << "Error: use ./ircserv <port> <password>" << std::endl;
         return 1;
     }
 
@@ -25,17 +25,21 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
 
+    std::string password = argv[2];
+    if (password.empty()) {
+        std::cerr << "Error: usage is " << argv[0] << " <" << argv[1] << "> <"
+                  << argv[2] << ">" << std::endl;
+        exit(EXIT_FAILURE);
+    }
     Server server(port, argv[2]);
-	server.initPoll();
+    server.initPoll();
 
     signal(SIGINT, sigHandler);
 
-
-	while (powerOn)
-	{
-		server.run();
-	}
-	server.closeSockets();
-	server.shutDown();
+    while (powerOn) {
+        server.run();
+    }
+    server.closeSockets();
+    server.shutDown();
     return 0;
 }
